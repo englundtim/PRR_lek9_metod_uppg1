6307b8341e03e0320714667ce161563eaf9d191f
Tim <tim.englund@elev.ga.ntig.se>
2023-11-20 09:33:51 +0100

    uppg a klar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Uppg a)");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Vad sak din radie vara?");
+        double radie = in.nextDouble();
+        omkretsCirkel(radie);
+        System.out.println("omkrets vara:"+omkretsCirkel(radie));
     }
+
+        static double omkretsCirkel(double radie)
+        {
+            return radie*2*3.14;
+        }
+
 }
